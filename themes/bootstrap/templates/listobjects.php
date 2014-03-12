2227a19191dcec8760d1d409ab0127a55e36146e
jchome <julien.coron@gmail.com>
2014-03-12 18:25:00 +0100

    Correction de la pagination

--- themes/bootstrap/templates/listobjects.php
+++ themes/bootstrap/templates/listobjects.php
@@ -49,25 +49,25 @@ RETURN = allAttributeCode
 		$data['asc'] = $asc;
 		
 		// preparer la pagination
-		$config['base_url'] = base_url().'index.php/list%%(self.obName.lower())%%s/index/'.$orderBy.'/'.$asc.'/';
+		$config['base_url'] = base_url().'index.php/%%(self.obName.lower())%%/list%%(self.obName.lower())%%s/index/'.$orderBy.'/'.$asc.'/';
 		$config['total_rows'] = %%(self.obName)%%_model::getCount%%(self.obName)%%s($this->db);
 		$config['per_page'] = 15;
 		$config['cur_tag_open'] = '<li class="active"><a href="#">';
 		$config['cur_tag_close'] = '</a></li>';
 		$config['num_tag_open'] = '<li>';
 		$config['num_tag_close'] = '</li>';
-		$config['prev_tag_open'] = '<li class="prev>';
+		$config['prev_tag_open'] = '<li class="prev">';
 		$config['prev_tag_close'] = '</li>';
 		$config['next_tag_open'] = '<li class="next">';
 		$config['next_tag_close'] = '</li>';
 		$config['first_link'] = '&lt;&lt;';
 		$config['first_tag_open'] = '<li>';
-		$config['first_tag_close'] = '</li>';		
+		$config['first_tag_close'] = '</li>';
 		$config['last_link'] = '&gt;&gt;';
 		$config['last_tag_open'] = '<li>';
-		$config['last_tag_close'] = '</li>';	
+		$config['last_tag_close'] = '</li>';
 		$config['num_links'] = 5;
-		$config['uri_segment'] = '5'; // where the page number is in the URI segment 
+		$config['uri_segment'] = '6'; // where the offset is in the URI segment 
 		$this->pagination->initialize($config);
 		$data['pagination'] = $this->pagination;
 		
