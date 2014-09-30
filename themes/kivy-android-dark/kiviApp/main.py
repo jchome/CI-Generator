66bc3da2e95c0e6112203ab709e8bf2e454ee532
jchome <julien.coron@gmail.com>
2014-09-30 18:36:40 +0200

    Debug des appels

--- themes/kivy-android-dark/kiviApp/main.py
+++ themes/kivy-android-dark/kiviApp/main.py
@@ -17,49 +17,17 @@ __version__ = '1.5'
 # adb logcat -s "python"
 
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+from kivy.uix.screenmanager import SlideTransition
+from screens.customscreen import CustomScreenManager, CustomScreen
 
 ### import des entités
 #from clients import ClientsApp
 
-class CustomScreenManager(ScreenManager):
-	def __init__(self, **kwargs):
-		super(ScreenManager, self).__init__(**kwargs)
-		self.allScreens = []
-		self.screenIndex = 0
-		Window.bind(on_keyboard=self.hook_keyboard)
-		
-	def hook_keyboard(self, window, key, *largs):
-		if key == 27: # BACK
-			#print("BACK")
-			return self.back()
-		elif key in (282, 319): # SETTINGS
-			# Irrelevant code
-			print("SETTINGS")
 	
-	def add_screen(self, aScreen):
-		self.allScreens.append(aScreen.name)
-		self.add_widget(aScreen)
-		
-	def next(self):
-		self.screenIndex = self.screenIndex + 1
-		self.transition = SlideTransition(direction="left")
-		self.current = self.allScreens[self.screenIndex]
-		return self.current_screen
-	
-	def back(self):
-		if self.screenIndex == 0:
-			return False
-		self.screenIndex = self.screenIndex - 1
-		self.transition = SlideTransition(direction="right")
-		self.current = self.allScreens[self.screenIndex]
-		self.current_screen.postback()
-		return True
-	
-class Welcome(Screen):
+class Welcome(CustomScreen):
 
 	def do_enter(self):
-		self.manager.goNext()
+		self.manager.go_next()
 		### passer a l'écran clients
 		#aJsonData = ...
 		#self.manager.get_screen("Clients").setItems( aJsonData )
