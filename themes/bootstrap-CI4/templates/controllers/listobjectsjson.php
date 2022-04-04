f5652f307de1c8a48c7f97047d03f0582f8f852a
Julien CORON <julien.coron@gmail.com>
2022-04-04 19:47:40 +0200

    Correction on json access

--- themes/bootstrap-CI4/templates/controllers/listobjectsjson.php
+++ themes/bootstrap-CI4/templates/controllers/listobjectsjson.php
@@ -6,27 +6,37 @@
  * Created by generator
  *
  */
+namespace App\Controllers\%%(self.obName.title())%%;
+
+use CodeIgniter\API\ResponseTrait;
 
 class List%%(self.obName)%%sJson extends \App\Controllers\BaseController {
 
-	/**
-	 * Constructeur
-	 */
-	function __construct(){
-		parent::__construct();
-		$this->load->model('%%(self.obName)%%Model');
-		$this->load->library('%%(self.obName)%%Service');
-		$this->load->library('session');
-		$this->load->database();
-	}
+	use ResponseTrait;
 
 	/**
 	 * Affichage des %%(self.obName)%%s
 	 */
-	public function index(){
+	public function index($orderBy = '%%(self.nonKeyFields[0].dbName)%%', $asc = 'asc', $offset = 0){
+		helper(['database']);
+
+		// preparer le tri
+		$data['orderBy'] = $orderBy;
+		$data['asc'] = $asc;
+		$limit = 10;
+		$pager = \Config\Services::pager();
 		// recuperation des donnees
-		$data['data'] = $this->%%(self.obName.lower())%%service->getAll($this->db);
-		$this->load->view('json/jsonifyData_view', $data);
+		$this->%%(self.obName.lower())%%Model = new \App\Models\%%(self.obName.title())%%Model();
+
+		$data['%%(self.obName.lower())%%s'] = $this->%%(self.obName.lower())%%Model
+			->orderBy($orderBy, $asc)->paginate($limit, 'bootstrap', null, $offset);
+		$data['pager'] = $this->%%(self.obName.lower())%%Model->pager;
+
+		return $this->respond([
+			'text' => 'ok',
+			'data' => $data
+		]);
+
 	}
 
 %%allAttributeCode = ""
@@ -37,11 +47,16 @@ for field in self.fields:
 	else:
 		attributeCode += """
 	public function findBy_%(fieldDbName)s($%(fieldDbName)s, $orderBy = null, $limit = 50, $offset = 0){
-		$asc = null;
-		$data['data'] = $this->%(objectNameLower)sservice->getAllBy_%(fieldDbName)s($this->db, urldecode($%(fieldDbName)s), $orderBy, $asc, $limit, $offset);
-		$this->load->view('json/jsonifyData_view', $data);
+		// recuperation des donnees
+		$this->%(objectNameLower)sModel = new \App\Models\%(objectNameTitle)sModel();
+		$model = $this->%(objectNameLower)sModel->find($%(fieldDbName)s);
+		return $this->respond([
+			'text' => 'ok',
+			'data' => $model
+		]);
 	}""" % { 'fieldDbName' : field.dbName.lower(),
 			'objectNameLower' : self.obName.lower(),
+			'objectNameTitle' : self.obName.title(),
 			'obName' : self.obName
 		}
 		
@@ -51,47 +66,6 @@ for field in self.fields:
 RETURN = allAttributeCode
 %%
 
-%%allAttributeCode = ""
-for field in self.fields:
-	attributeCode = ""
-	if field.sqlType.upper()[0:4] == "FILE":
-		continue
-	else:
-		attributeCode += """
-	public function countBy_%(fieldDbName)s($%(fieldDbName)s){
-		$data['data'] = $this->%(objectNameLower)sservice->countBy_%(fieldDbName)s($this->db, urldecode($%(fieldDbName)s));
-		$this->load->view('json/jsonifyData_view', $data);
-	}""" % { 'fieldDbName' : field.dbName.lower(),
-			'objectNameLower' : self.obName.lower()
-		}
-		
-	if attributeCode != "":
-		allAttributeCode += attributeCode
-	
-RETURN = allAttributeCode
-%%
-
-
-%%allAttributeCode = ""
-for field in self.fields:
-	attributeCode = ""
-	if field.sqlType.upper()[0:4] == "FILE":
-		continue
-	else:
-		attributeCode += """
-	public function countBy_%(fieldDbName)sGET(){
-		$%(fieldDbName)s = $this->input->get('%(fieldDbName)s');
-		$data['data'] = $this->%(objectNameLower)sservice->countBy_%(fieldDbName)s($this->db, urldecode($%(fieldDbName)s));
-		$this->load->view('json/jsonifyData_view', $data);
-	}""" % { 'fieldDbName' : field.dbName.lower(),
-			'objectNameLower' : self.obName.lower()
-		}
-
-	if attributeCode != "":
-		allAttributeCode += attributeCode
-
-RETURN = allAttributeCode
-%%
 
 
 %%allAttributeCode = ""
@@ -99,10 +73,10 @@ for field in self.fields:
 	attributeCode = ""
 	if field.sqlType.upper()[0:7] == "VARCHAR" or field.sqlType.upper()[0:4] == "TEXT" :
 		attributeCode += """
-	public function findLike_%(fieldDbName)s($%(fieldDbName)s){
+	/*public function findLike_%(fieldDbName)s($%(fieldDbName)s){
 		$data['data'] = $this->%(objectNameLower)sservice->getAllLike_%(fieldDbName)s($this->db, urldecode($%(fieldDbName)s));
 		$this->load->view('json/jsonifyData_view', $data);
-	}""" % { 'fieldDbName' : field.dbName.lower(),
+	}*/""" % { 'fieldDbName' : field.dbName.lower(),
 			'objectNameLower' : self.obName.lower()
 		}
 		
